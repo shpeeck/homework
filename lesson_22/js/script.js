1063463192f3f5023eb296e42c66e10642e7fc3f
shpeeck <jenya_vas@mail.ru>
2020-11-08 10:07:23 +0200

    homework lesson 22 border radius

--- lesson_22/js/script.js
+++ lesson_22/js/script.js
@@ -19,32 +19,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     });
 
+    // Получаем Range и блок
+    // console.dir(document.querySelector(".border-lt"));
+    let topLeft = document.querySelector(".border-lt");
+    let topRight = document.querySelector(".border-rt");
+    let center = document.querySelector(".center");
+    let bottomLeft = document.querySelector(".border-lb");
+    let bottomRight = document.querySelector(".border-rb");
 
+    topLeft.addEventListener('change', function (e) {
+        center.style.borderTopLeftRadius = topLeft.value + 'px';
+        // Второй вариант 
+        // center.setAttribute("style", `border-top-left-radius: ${topLeft.value}px`);
+        console.log(topLeft);
+    });
+    topRight.addEventListener('change', function (e) {
+        center.style.borderTopRightRadius = topRight.value + 'px';
+    });
+    bottomLeft.addEventListener('change', function (e) {
+        center.style.borderBottomLeftRadius = bottomLeft.value + 'px';
+    });
+    bottomRight.addEventListener('change', function (e) {
+        center.style.borderBottomRightRadius = bottomRight.value + 'px';
+    });
 });
-
-
-
-// Из инета
-// html 
-{/* <div class="code_color">#ffffff</div>
-<button onclick="randColor('.example')">Нажми</button> */}
-
-// js 
-
-// function randColor(elem) {
-    /*СОЗДАЕМ ПЕРЕМЕННЫЕ
-    elem - элемент которому будем менять задний фон
-    code_color - получаем элемент в который будет выводить код цвета
-    r,g,b - отвечают за кодировку и вместе выводят цвет
-    color - в нее записываем полную строку значения цвета
-    */
-            //     var element = document.querySelector(elem),
-            //         code_color = document.querySelector('.code_color'),
-            //         r = Math.floor(Math.random() * (256)),
-            //         g = Math.floor(Math.random() * (256)),
-            //         b = Math.floor(Math.random() * (256)),
-            //         color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-    
-            //     element.style.background = color;
-            //     code_color.innerText = color;
-            // }
